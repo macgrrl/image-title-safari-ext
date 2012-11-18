52364c07b5dc7846242b29e0af33a13f2edb55cb
Kirschen Seah <kirschen@freerangecoder.com>
2012-11-18 09:40:28 -0600

    Added Image Title.safariextz to the ignore list. Revised version to 1.1. Updated image_title_inject.js to use getAttributes instead of attributes.getNamedItem().value, and combines alt text and title text if both exist.

--- Image Title.safariextension/image_title_inject.js
+++ Image Title.safariextension/image_title_inject.js
@@ -4,6 +4,8 @@
  * Adds the "title" attribute to an image tag if there is "alt" text
  * This will cause Safari to show a popup of the alt text
  *
+ * If there's title text in addition to the alt text, then merge the two together
+ *
  * Image Title by Kirschen Seah (http://www.freerangecoder.com)
  * is licensed under a
  * Creative Commons Attribution-NonCommercial 3.0 Unported License
@@ -18,15 +20,25 @@ var imageCollection = document.images;
 for ( var i = 0; i < imageCollection.length; ++i )
 {
 	var theNode = imageCollection.item( i );
-	var altText = theNode.attributes.getNamedItem("alt").value;
+	var altText = theNode.getAttribute("alt");
 
 	// if we have alt text
 	if ( altText != "" )
 	{
-		theNode.setAttribute("title",altText);
-			// add the title attribute
+		var titleText = theNode.getAttribute("title");
+
+		if ( titleText == null )
+		{
+			theNode.setAttribute("title",altText);
+			// add the altText as title attribute
 			// so safari will show the popup
 			// NOTE: safari webkit DOM says that a Node doesn't have setAttribute
 			// but I'm guessing that imageCollection.item returns an Element
+		}
+		else if ( titleText != "" )
+		{
+			theNode.setAttribute("title", "Alt text: " + altText + "\r\nTitle: " + titleText);
+			// put both alt and title text into title attribute
+		}
 	}
 }
